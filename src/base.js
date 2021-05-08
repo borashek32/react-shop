a23b76b157b726b24f0b8640bb1dace9610d8e71
Наташа Баранова <natasabaranova@MacBook-Pro-Natasa.local>
2021-05-08 14:20:59 +0300

    firebase

--- src/base.js
+++ src/base.js
@@ -1,7 +1,7 @@
 import Rebase from 're-base';
 import firebase from 'firebase/app';
 
-const firebaseApp = firebase.initializeApp({
+const firebaseApp = firebase.initializeApp90({
   apiKey: "AIzaSyCKet1oFBWyjNjzGN5b0-hVyDf0ViV1gPM",
   authDomain: "very-hot-burgers-7a2a2.firebaseapp.com",
   databaseURL: "https://very-hot-burgers-7a2a2-default-rtdb.firebaseio.com",
@@ -9,15 +9,6 @@ const firebaseApp = firebase.initializeApp({
 
 const base = Rebase.createClass(firebaseApp.database());
 
-function initFirebase() {
-  if (!firebase.apps.length) {
-    firebase.initializeApp(config);
-  }
-}
-
-initFirebase();
-
 export { firebaseApp };
 
 export default base;
-
